39fa3b25fea02c87206069e65f88c3bb6297510e
Muhammad Syah Rizal <79929326+syahrizzal06@users.noreply.github.com>
2021-04-10 11:12:32 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	
-	$koneksi = mysqli_connect("localhost","id15651729_root","Moderen001!!","id15651729_relawan" );
-
-	if (!$koneksi) {
-		die("Gagal Terhubung dengan database".mysqli_connect_error());
-	}
-
- ?>
